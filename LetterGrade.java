a32fd26b435d5bc7492bd07253d4a036d628f95e
Adithya2106 <146044635+Adithya2106@users.noreply.github.com>
2023-10-02 13:01:28 -0500

    Add files via upload

---
+++ LetterGrade.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class LetterGrade {
+    public static void main(String[] args) {
+        String letterGrade;
+
+        System.out.println("Please enter your grade : ");
+        Scanner scan = new Scanner(System.in);
+        int grade = scan.nextInt();
+        if (grade >= 95) {
+            letterGrade = "A+";
+        } else if (grade >= 90) {
+            letterGrade = "A";
+        } else if (grade >= 85) {
+            letterGrade = "B+";
+        } else if (grade >= 80) {
+            letterGrade = "B";
+        } else if (grade >= 75) {
+            letterGrade = "C+";
+        } else if (grade >= 70) {
+            letterGrade = "C";
+        } else if (grade >= 65) {
+            letterGrade = "D+";
+        } else if (grade >= 60) {
+            letterGrade = "D";
+        } else {
+            letterGrade = "F";
+        }
+
+        System.out.println("The Final Grade is : " + letterGrade);
+    }
+}
